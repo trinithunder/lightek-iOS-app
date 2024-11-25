52e9ff6b133b7fe873f6b11251ba3a6b88ebfa0d
Marlon <marlon@Marlons-MacBook-Pro.local>
2024-11-25 14:43:08 -0500

    Added tab bar view to home so now all root pages can be displayed, also added in auth check value that launches the auth path

--- Lightek/Lightek/Shared/ContentView.swift
+++ Lightek/Lightek/Shared/ContentView.swift
@@ -8,9 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var selection = 0
+    @State var isUserLoggedIn = false
+    @State var userNeedsToAuth = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+       TabView(selection: $selection) {
+            HomeView().tabItem { Text("Home") }.tag(1)
+            Text("Orders").tabItem { Text("Orders") }.tag(2)
+           Text("Chats").tabItem { Text("Chats") }.tag(3)
+           
+           Text("Profile").tabItem { Text("Profile") }.tag(4)
+       }.fullScreenCover(isPresented: $userNeedsToAuth) {
+           //Figure out what to do when the user is finally authed here
+       } content: {
+           Text("Please sign the fuck in")
+       }
+
     }
 }
 
